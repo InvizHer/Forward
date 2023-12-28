cdb9ac552078c3fb29bc181e3eb93ce40dcc830c
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-12-28 20:52:25 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -11,11 +11,11 @@ def is_enabled(value, default):
         return default
 
 SESSION = environ.get("SESSION", "forward bot")
-API_ID = int(environ.get("API_ID", 10098309))
-API_HASH = environ["API_HASH", "aaacac243dddc9f0433c89cab8efe323"]
-BOT_TOKEN = environ["BOT_TOKEN", "5473067352:AAER3bMhPjiNZQmkkU9LJSIX5BiH7WuA2z8"]
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", -1001505526419))
+API_ID = int(environ["API_ID"])
+API_HASH = environ["API_HASH"]
+BOT_TOKEN = environ["BOT_TOKEN"]
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", 0))
 PORT = int(environ.get("PORT", "8080"))
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '2056407064').split()]
+ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '').split()]
 TARGET_DB = int(environ.get("TARGET_DB", 0))
 UPSTREAM_REPO = environ.get("UPSTREAM_REPO", "https://github.com/InvizHer/Forward")
