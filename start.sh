72e1fbcc246f8adda1c10422056c505e63d91362
InvizHer <112791490+InvizHer@users.noreply.github.com>
2023-12-28 18:56:21 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/InvizHer/Piro-forword.git /Forward
+  git clone https://github.com/InvizHer/Forward.git /Forward
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /Piro-Forward
+  git clone $UPSTREAM_REPO /Forward
 fi
 cd /Forward
 pip3 install -U -r requirements.txt
